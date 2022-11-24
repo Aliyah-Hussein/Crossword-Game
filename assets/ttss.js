7292c6a5a16fb19f20fe5ee36a33fddac2c5793f
Aliyah-Hussein <aliyah.hussein0298@gmail.com>
2022-11-25 11:08:04 +1300

    first commit

---
+++ assets/ttss.js
@@ -0,0 +1,72 @@
+var ttss = [
+	{
+		"word":"boolean",
+		"clue":"Is a Primative datatype"
+	},
+	{
+		"word":"array",
+		"clue":"Is a derived datatype"
+	},
+	{
+		"word":"class",
+		"clue":"Is a abstract or user-derived datatype"
+	},
+	{
+		"word":"calculator",
+		"clue":"A tool to calculate."
+	},
+	{
+		"word":"signed",
+		"clue":"modifiers in c++"
+	},
+	{
+		"word":"cin",
+		"clue":"means character input"
+	},
+	{
+		"word":"bit",
+		"clue":" variable quantity capable of holding exactly one of two possible values"
+	},
+	{
+		"word":"int",
+		"clue":"basic built in type refers to integer"
+	},
+	{
+		"word":"object",
+		"clue":"A contiguous region of memory with a type"
+	},
+	
+	{
+		"word":"parse",
+		"clue":"To break a program, statement, or expression into its component parts."
+	},
+	{
+		"word":"void",
+		"clue":"_____ is essentially the absence of any translation rule or storage size"
+	},
+	{
+		"word":"double",
+		"clue":"similar to a float"
+	},
+	{
+		"word":"bool",
+		"clue":"A tool to calculate."
+	},
+	{
+		"word":"identifier",
+		"clue":"A sequence of characters associated with and used to access a particular object, class or struct, or function"
+	},
+	{
+		"word":"scope",
+		"clue":"Every identifier has a ______"
+	},
+	{
+		"word":"computer",
+		"clue":"A tool to compute"
+	},
+	{
+		"word":"statement",
+		"clue":"An expression followed by a semi-colon."
+	},
+	
+]
